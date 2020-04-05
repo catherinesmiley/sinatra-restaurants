3d7242c319487090adfb56c33b9c120145dfe5e4
cesmiley <catherinesmiley@gmail.com>
2020-04-05 18:44:11 -0400

    created restaurants get and post routes, restaurants/new get route, and added form to restaurants new page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,20 @@
 class RestaurantsController < ApplicationController
 
+    get '/restaurants' do 
+        @restaurants = Restaurant.all 
+        erb :'/restaurants/index'
+    end 
+
+    post '/restaurants' do 
+        restaurant = Restaurant.create(params)
+        user = Helpers.current_user(session)
+        restaurant.user = user 
+        restaurant.save 
+        redirect to "/users/#{user.id}"
+    end 
+
+    get '/restaurants/new' do 
+        erb :'/restaurants/new'
+    end 
+
 end 
